29da204613840f4103b88237dd823378025527f8
andro <andro999b@gmail.com>
2016-11-14 15:49:34 +0200

    fixes

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from enum import Enum
 
 
@@ -11,19 +13,22 @@ class FoodType(Enum):
 prefered_food = [
     ("окрошка с курицей", FoodType.appetizer, 1),
     ("cуп пюре из цукини", FoodType.appetizer, 1.4),
-    ("cуп гороховый", FoodType.appetizer, 1.2),
+    ("cуп гороховый", FoodType.appetizer, 1),
     ("пюре картофельное", FoodType.main_course, 1),
     ("гречка", FoodType.main_course, 1),
     ("рис", FoodType.main_course, 1),
     ("рис с маслом", FoodType.main_course, 1),
-    ("куриные треугол", FoodType.garnish, 1),
+    ("плов с курицей", FoodType.main_course, 1),
+    ("куриные треугол", FoodType.garnish, 1.5),
     ("зразы с мясом", FoodType.garnish, 1),
-    ("нагетсы", FoodType.garnish, 0.8),
+    ("нагетсы куриные", FoodType.garnish, 1),
     ("котлета куриная", FoodType.garnish, 1),
     ("котлета по-киевски", FoodType.garnish, 1),
+    ("котлета фаршированая сыром", FoodType.garnish, 1),
     ("эскалоп с овощами", FoodType.garnish, 1),
     ("эскалоп под сыроми", FoodType.garnish, 1),
     ("говядина в соусе", FoodType.garnish, 1),
+    ("свинина в соусе", FoodType.garnish, 1),
     ("филе куриное с помидором", FoodType.garnish, 1),
     ("голубцы с мясом и рисом", FoodType.garnish, 0)
 ]
--- spreadsheet_menu.py
+++ spreadsheet_menu.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import httplib2
 import day_menu
 import string
